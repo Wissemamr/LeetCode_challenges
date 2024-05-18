9b58e21a318f9a015defa9981ed9810a9c67eb8c
wissemamr <kw_amar@esi.dz>
2024-05-18 23:56:26 +0200

    added greatets common string dividor

---
+++ Leetcode75/greatest_common_string_divisor..py
@@ -0,0 +1,17 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        def gcd(a, b):
+            ''' euclidean algortihm'''
+            while b:
+                a, b = b, a % b
+            return a
+        if str1 + str2 != str2 + str1:
+            return ""
+        gcd_len = gcd(len(str1), len(str2))
+        return str1[:gcd_len]
+    
+    
+if __name__ == "__main__":
+    sol = Solution()
+    answer = sol.gcdOfStrings("ABCABC", "ABC")
+    print(f"The answer is : {answer}")
