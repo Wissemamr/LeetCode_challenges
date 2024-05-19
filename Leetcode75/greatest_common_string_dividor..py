1b7590829187d305a14a94c678df5782232e16a5
wissemamr <kw_amar@esi.dz>
2024-05-19 22:14:09 +0200

    black formatting

--- Leetcode75/greatest_common_string_dividor..py
+++ Leetcode75/greatest_common_string_dividor..py
@@ -1,16 +1,17 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         def gcd(a, b):
-            ''' euclidean algortihm'''
+            """euclidean algortihm"""
             while b:
                 a, b = b, a % b
             return a
+
         if str1 + str2 != str2 + str1:
             return ""
         gcd_len = gcd(len(str1), len(str2))
         return str1[:gcd_len]
-    
-    
+
+
 if __name__ == "__main__":
     sol = Solution()
     answer = sol.gcdOfStrings("ABCABC", "ABC")
--- Other_challenges/intro_to_pandas.py
+++ Other_challenges/intro_to_pandas.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 # Q1 : Create a dataframe from a list of lists
 def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
-    ids = [student[0] for student in student_data] 
-    ages = [student[1] for student in student_data] 
-    student_dict = {'student_id' : ids, 'age': ages}
+    ids = [student[0] for student in student_data]
+    ages = [student[1] for student in student_data]
+    student_dict = {"student_id": ids, "age": ages}
     return pd.DataFrame(student_dict)
 
 
@@ -22,69 +22,76 @@ def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
 
 # Q4 : select data from a df
 def selectData(students: pd.DataFrame) -> pd.DataFrame:
-    return students.loc[students['student_id']==101,['name', 'age']]
+    return students.loc[students["student_id"] == 101, ["name", "age"]]
 
 
 # Q5 : create a new column
 def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
-    employees['bonus']=employees['salary']*2
+    employees["bonus"] = employees["salary"] * 2
     return employees
 
 
 # Q6 : drop duplicates
 def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
-    customers = customers.drop_duplicates(subset=['email'])
+    customers = customers.drop_duplicates(subset=["email"])
     return customers
 
 
 # Q7 : drop missing data
 def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
-    students =students.dropna(subset=['name'])
+    students = students.dropna(subset=["name"])
     return students
-    
-    
+
+
 # Q8 : modify columns
 def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
-    employees['salary'] = employees['salary']*2
+    employees["salary"] = employees["salary"] * 2
     return employees
 
 
 # Q9 : rename columns
 def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
-    students.rename(columns={'id':'student_id', 'first':'first_name', 'last':'last_name','age':'age_in_years'}, inplace=True)
+    students.rename(
+        columns={
+            "id": "student_id",
+            "first": "first_name",
+            "last": "last_name",
+            "age": "age_in_years",
+        },
+        inplace=True,
+    )
     return students
 
 
 #  Q10 : change data type
 def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
-    students['grade'] = students.grade.astype(int)
+    students["grade"] = students.grade.astype(int)
     return students
-    
-    
+
+
 # Q11 : fill missing data
 def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
-    products.fillna({'quantity':0}, inplace=True)
+    products.fillna({"quantity": 0}, inplace=True)
     return products
 
 
 # Q12 : seshape data : concat
 def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
-    return pd.concat([df1,df2], axis=0)
-
+    return pd.concat([df1, df2], axis=0)
 
 
 # Q13 : reshape data : pivot
 def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
-    return weather.pivot(index='month', columns='city', values='temperature')
-    
-    
+    return weather.pivot(index="month", columns="city", values="temperature")
+
+
 #  Q14 : reshape data : melt
 def meltTable(report: pd.DataFrame) -> pd.DataFrame:
-    return pd.melt(report,id_vars=['product'],var_name='quarter',value_name='sales')
+    return pd.melt(report, id_vars=["product"], var_name="quarter", value_name="sales")
+
 
 # Q15 : method chaining
 def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
-    return animals[animals['weight']>100].sort_values(by='weight', ascending=False)[['name']]
-    
-
-
+    return animals[animals["weight"] > 100].sort_values(by="weight", ascending=False)[
+        ["name"]
+    ]
